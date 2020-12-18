12b51cf5b0cf90c14ebd20fa958f2843e7b61784
Arkasha99 <GreyForm16@gmail.com>
2020-12-18 19:45:58 +0600

    Создал репозитории всех мастей

---
+++ src/main/java/com/lyamin/repository/FilmRatingRepository.java
@@ -0,0 +1,8 @@
+package com.lyamin.repository;
+
+import com.lyamin.pojo.FilmRating;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FilmRatingRepository extends CrudRepository<FilmRating,Long> {
+
+}
